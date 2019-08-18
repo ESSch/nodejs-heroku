e2ac652d29c3b92df2a822cd992e41a8f0101740
ESSch <ESSchtolts@yandex.ru>
2019-08-19 00:26:39 +0300

    version 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 require('http').createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Success\n');
+    res.end('Seuccess 2\n');
 }).listen(process.env.PORT || 8000, "0.0.0.0");
